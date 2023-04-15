4a9f86a31d5f020eeaf483e78fe058b691e70b60
wooniekim <dawun3295@gmail.com>
2023-04-16 02:03:22 +0900

    add calendar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -138,6 +138,15 @@ export function Navbar({ user }: UserProps) {
               </NavLink>
             </li>
             <li>
+              <NavLink
+                to="/calendar"
+                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
+                aria-current="page"
+              >
+                캘린더
+              </NavLink>
+            </li>
+            <li>
               {user !== undefined ? (
                 <NavLink
                   to="/custom"
